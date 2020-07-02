4ce369ddd60ae1b5f7e514a3693103593de72c72
Sadman Shawmik <hello@sadmansh.com>
2020-07-03 05:17:58 +0600

    Add all task routes

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -6,37 +6,56 @@ const Task = mongoose.model('Task')
 
 router.post('/tasks/add', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const task = req.body
+		const newTask = await new Task({
+			start: task.start,
+			details: task.details,
+			end: task.end
+		}).save()
+		return res.send(newTask)
 	} catch (error) {
 		console.error(error)
-		return res.status(200).send({ error: error })
+		return res.status(200).send({ error: error.message })
 	}
 })
 
 router.get('/tasks', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const tasks = await Task.find({ _user: req.user.id })
+		return res.send(tasks.reverse())
 	} catch (error) {
 		console.error(error)
-		return res.status(200).send({ error: error })
+		return res.status(200).send({ error: error.message })
 	}
 })
 
 router.get('/tasks/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const task = await Task.findOne({ _id: req.params.id, _user: req.user.id })
+		return res.send(task)
 	} catch (error) {
 		console.error(error)
-		return res.status(200).send({ error: error })
+		return res.status(200).send({ error: error.message })
 	}
 })
 
 router.put('/tasks/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const task = await Task.findOneAndUpdate({ _id: req.params.id, _user: req.user.id }, { ...req.body }, { new: true }).exec()
+		return res.send(task)
 	} catch (error) {
 		console.error(error)
-		return res.status(200).send({ error: error })
+		return res.status(200).send({ error: error.message })
+	}
+})
+
+router.delete('/tasks/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+	try {
+		const task = await Task.findOneAndDelete({ _id: req.params.id, _user: req.user.id })
+		return res.status(200).send(req.params.id)
+	} catch (error) {
+		console.error(error)
+		return res.status(200).send({ error: error.message })
 	}
 })
 
