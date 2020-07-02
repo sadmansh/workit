4a7dbafa9b18c75693e5e23fdce77f1d948bc8cd
Sadman Shawmik <hello@sadmansh.com>
2020-07-03 05:59:53 +0600

    Add all entry routes

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -6,7 +6,12 @@ const Entry = mongoose.model('Entry')
 
 router.post('/entries/add', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const entry = req.body
+		const newEntry = await new Entry({
+			signIn: entry.signIn,
+			signOut: entry.signOut
+		}).save()
+		return res.send(newEntry)
 	} catch (error) {
 		console.error(error)
 		return res.status(200).send({ error: error })
@@ -15,7 +20,8 @@ router.post('/entries/add', passport.authenticate('jwt', { session: false }), as
 
 router.get('/entries', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const entries = await Entry.find({ _user: req.user.id })
+		return res.send(entries.reverse())
 	} catch (error) {
 		console.error(error)
 		return res.status(200).send({ error: error })
@@ -24,10 +30,31 @@ router.get('/entries', passport.authenticate('jwt', { session: false }), async (
 
 router.get('/entries/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
 	try {
-		return
+		const entry = await Entry.findOne({ _id: req.params.id, _user: req.user.id })
+		return res.send(entry)
 	} catch (error) {
 		console.error(error)
 		return res.status(200).send({ error: error })
+	}
+})
+
+router.put('/entries/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+	try {
+		const entry = await Entry.findOneAndUpdate({ _id: req.params.id, _user: req.user.id }, { ...req.body }, { new: true }).exec()
+		return res.send(entry)
+	} catch (error) {
+		console.error(error)
+		return res.status(200).send({ error: error })
+	}
+})
+
+router.delete('/entries/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+	try {
+		const entry = await Entry.findOneAndDelete({ _id: req.params.id, _user: req.user.id })
+		return res.status(200).send(req.params.id)
+	} catch (error) {
+		console.error(error)
+		return res.status(200).send({ error: error.message })
 	}
 })
 
