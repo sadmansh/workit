d5418160fd0649718aaf6a08d0ab968fba46accf
Sadman Shawmik <hello@sadmansh.com>
2020-07-03 09:50:10 +0600

    Add button to sign out for the day

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import * as actions from '../actions'
 
 import SignIn from './Entry/SignIn'
+import SignOut from './Entry/SignOut'
 import AddTask from './Task/AddTask'
 import TasksList from './Task/TasksList'
 // import SignOut from './Entry/SignOut'
@@ -24,9 +25,10 @@ const Dashboard = () => {
 			<div>Hello, {user.firstName}!</div>
 			<SignIn entries={entries} setEntry={setEntry} />
 
-			{entry.length ? 
+			{entry.length && entries.length ? 
 				<div className="tasks">
 					<AddTask entry={entry} />
+					<SignOut entries={entries} />
 					<TasksList entry={entry} />
 				</div>
 				: ''
---
+++ client/src/components/Entry/SignOut.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import * as actions from '../../actions'
+import moment from 'moment'
+
+window.moment = moment
+
+const SignOut = props => {
+	const [id, setId] = useState('')
+	const [stop, setStop] = useState('')
+	const { entries } = props
+	const dispatch = useDispatch()
+
+	useEffect(() =>{
+		if (entries) {
+			entries.forEach(entry => {
+				let signInDate = moment(entry.signIn)
+				if (signInDate.isSame(moment(Date.now()), 'day')) {
+					setId(entry._id)
+					if (entry.signOut) setStop(entry.signOut)
+				}
+			})
+		}
+	}, [props])
+
+	const stopEntry = () => {
+		let stopDate = Date.now()
+		setStop(stopDate)
+		dispatch(actions.updateEntry(id, { signOut: stopDate }))
+	}
+	return (
+		<div>
+			{!stop ? 
+				<button className="sign-out" onClick={stopEntry}>Sign Out</button>
+				:
+				<div>Signed out at {moment(stop).format('hh:mm A')}</div>
+			}
+		</div>
+	)
+}
+
+export default SignOut
