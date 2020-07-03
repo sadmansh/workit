e0ea55b2e3ed5bdd2e9f1775cc63114a9179978a
Sadman Shawmik <hello@sadmansh.com>
2020-07-03 08:27:20 +0600

    Add task component

---
+++ client/src/components/Task/AddTask.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import * as actions from '../../actions'
+
+const AddTask = props => {
+
+	const [task, setTask] = useState({
+		start: '',
+		end: '',
+		details: ''
+	})
+
+	const inputHandler = e => {
+		setTask({...task, [e.target.name]: e.target.value})
+	}
+	const formHandler = e => {
+		e.preventDefault()
+		console.log(task)
+		// dispatch(actions.createTask(task))
+		e.target.reset()
+	}
+
+	return (
+		<form onSubmit={formHandler} className="task-form">
+			<div className="task-meta">
+				<input type="time" name="start" onChange={inputHandler} />
+				<input type="time" name="end" onChange={inputHandler} />
+			</div>
+			<div>
+				<textarea name="details" onChange={inputHandler} />
+			</div>
+			<button type="submit">Add task</button>
+		</form>
+	)
+}
+
+export default AddTask
