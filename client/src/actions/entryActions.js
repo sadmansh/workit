79396a8dbff250ee4d24faa6c05438aee1b02ea1
Sadman Shawmik <hello@sadmansh.com>
2020-07-07 01:30:12 +0600

    Replace env vars with production api url

--- client/src/actions/entryActions.js
+++ client/src/actions/entryActions.js
@@ -3,13 +3,13 @@ import { FETCH_ENTRIES, CREATE_ENTRY, UPDATE_ENTRY, DELETE_ENTRY } from './types
 import AuthHeaders from '../utils/AuthHeaders'
 
 export const fetchEntries = () => async dispatch => {
-	const res = await axios.get(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/entries`, AuthHeaders)
+	const res = await axios.get(`https://ms-workit.herokuapp.com/api/entries`, AuthHeaders)
 	dispatch({ type: FETCH_ENTRIES, payload: res.data })
 }
 
 export const createEntry = signIn => async dispatch => {
 	try {
-		const res = await axios.post(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/entries/add`, { signIn }, AuthHeaders)
+		const res = await axios.post(`https://ms-workit.herokuapp.com/api/entries/add`, { signIn }, AuthHeaders)
 		dispatch({ type: CREATE_ENTRY, payload: res.data })
 	} catch (error) {
 		console.error(error)
@@ -21,7 +21,7 @@ export const updateEntry = (id, payload) => async dispatch => {
 		const entry = {}
 		if (payload.signIn) entry.signIn = payload.signIn
 		if (payload.signOut) entry.signOut = payload.signOut
-		const res = await axios.put(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/entries/${id}`, entry, AuthHeaders)
+		const res = await axios.put(`https://ms-workit.herokuapp.com/api/entries/${id}`, entry, AuthHeaders)
 		dispatch({ type: UPDATE_ENTRY, payload: res.data })
 	} catch (error) {
 		console.error(error)
@@ -30,7 +30,7 @@ export const updateEntry = (id, payload) => async dispatch => {
 
 export const deleteEntry = (id, payload) => async dispatch => {
 	try {
-		const res = await axios.delete(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/entries/${id}`, AuthHeaders)
+		const res = await axios.delete(`https://ms-workit.herokuapp.com/api/entries/${id}`, AuthHeaders)
 		dispatch({ type: DELETE_ENTRY, payload: res.data })
 	} catch (error) {
 		console.error(error)
--- client/src/actions/taskActions.js
+++ client/src/actions/taskActions.js
@@ -3,7 +3,7 @@ import { FETCH_TASKS, CREATE_TASK, UPDATE_TASK, DELETE_TASK } from './types'
 import AuthHeaders from '../utils/AuthHeaders'
 
 export const fetchTasks = entry => async dispatch => {
-	const res = await axios.get(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/tasks`, { params: { entry }, ...AuthHeaders })
+	const res = await axios.get(`https://ms-workit.herokuapp.com/api/tasks`, { params: { entry }, ...AuthHeaders })
 	dispatch({ type: FETCH_TASKS, payload: res.data })
 }
 
@@ -14,7 +14,7 @@ export const createTask = task => async dispatch => {
 		if (task.end) payload.end = task.end
 		if (task.details) payload.details = task.details
 		if (task._entry) payload._entry = task._entry
-		const res = await axios.post(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/tasks/add`, payload, AuthHeaders)
+		const res = await axios.post(`https://ms-workit.herokuapp.com/api/tasks/add`, payload, AuthHeaders)
 		dispatch({ type: CREATE_TASK, payload: res.data })
 	} catch (error) {
 		console.error(error)
@@ -23,7 +23,7 @@ export const createTask = task => async dispatch => {
 
 export const updateTask = (id, payload) => async dispatch => {
 	try {
-		const res = await axios.put(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/tasks/${id}`, payload, AuthHeaders)
+		const res = await axios.put(`https://ms-workit.herokuapp.com/api/tasks/${id}`, payload, AuthHeaders)
 		dispatch({ type: UPDATE_TASK, payload: res.data })
 	} catch (error) {
 		console.error(error)
@@ -32,7 +32,7 @@ export const updateTask = (id, payload) => async dispatch => {
 
 export const deleteTask = id => async dispatch => {
 	try {
-		const res = await axios.delete(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/tasks/${id}`, AuthHeaders)
+		const res = await axios.delete(`https://ms-workit.herokuapp.com/api/tasks/${id}`, AuthHeaders)
 		dispatch({ type: DELETE_TASK, payload: res.data })
 	} catch (error) {
 		console.error(error)
