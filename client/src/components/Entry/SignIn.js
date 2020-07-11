c8fc12e45b93e136fc8ae84872e14a83df7c45ac
Sadman Shawmik <hello@sadmansh.com>
2020-07-12 01:56:28 +0600

    Add sign in edit feature

--- client/src/components/Entry/SignIn.js
+++ client/src/components/Entry/SignIn.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as actions from '../../actions'
 import moment from 'moment'
+import { PencilIcon } from '@primer/octicons-react'
 
 const SignIn = props => {
 	const [manualSignInFlag, setManualSignInFlag] = useState(false)
 	const [manualSignInTime, setManualSignInTime] = useState('')
+	const [editing, setEditing] = useState(false)
 	const dispatch = useDispatch()
 	const { entry } = props
 	
@@ -13,11 +15,35 @@ const SignIn = props => {
 		dispatch(actions.createEntry(time))
 	}
 
+	const editChangeHandler = e => {
+		setManualSignInTime(new Date(parseInt(moment(e.target.value, 'HH:mm').format('x'))).toISOString())
+	}
+
+	const updateSignIn = e => {
+		e.preventDefault()
+		dispatch(actions.updateEntry(entry._id, { signIn: manualSignInTime }))
+		setEditing(!editing)
+	}
+
 	return (
-		<div className="sign-in" style={{ textAlign: 'center' }}>
+		<div className={editing ? 'editing sign-in' : 'sign-in'} style={{ textAlign: 'center' }}>
 			{entry ? 
 				entry.signIn ? 
-					<span className="signed-in">You signed in at {moment(entry.signIn).format('hh:mm A')}</span>
+					<div>
+						{editing ? 
+							<form className="editing-form" onSubmit={updateSignIn}>
+								<input type="time" name="start" value={moment(manualSignInTime || entry.signIn).format('HH:mm')} onChange={editChangeHandler} />
+								<button type="submit">Save</button>
+							</form>
+							:
+							<div>
+								<span className="signed-in">You signed in at {moment(entry.signIn).format('hh:mm A')}</span>
+							</div>
+						}
+						<div className="edit-icon" onClick={() => setEditing(!editing)}>
+							<PencilIcon size={16} />
+						</div>
+					</div>
 					:
 					<div style={{ textAlign: 'center' }}>
 						<p>Start logging tasks by signing in first</p>
