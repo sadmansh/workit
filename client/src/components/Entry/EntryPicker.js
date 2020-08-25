ca94aa235f5fe6147c1708a220d12c1db901ea48
Sadman Shawmik <hello@sadmansh.com>
2020-08-25 10:07:58 +0600

    Create entry manually if it doesn't exist

--- client/src/components/Entry/EntryPicker.js
+++ client/src/components/Entry/EntryPicker.js
@@ -10,28 +10,36 @@ const EntryPicker = props => {
 	const { entries, history } = props
 	const [entry, setEntry] = useState({})
 	const [newEntry, setNewEntry] = useState(null)
+	const [error, setError] = useState(false)
+	const [noEntryDate, setNoEntryDate] = useState(null)
 	const dispatch = useDispatch()
 
 	const goToEntry = e => {
 		e.preventDefault()
+		setError(false)
 		if (entry) {
 			history.push(`/dashboard/entries/${entry._id}`)
 		} else {
-			axios.post(`https://ms-workit.herokuapp.com/api/entries/add`, { signIn: newEntry }, AuthHeaders).then(res => {
-				setEntry(res.data)
-				history.push(`/dashboard/entries/${res.data._id}`)
-				window.location.reload()
-			})
+			setError(true)
+			
 		}
 	}
 
 	const changeHandler = e => {
 		let date = new Date(parseInt(moment(e.target.value, 'YYYY:MM:DD').format('x'))).toISOString()
 		setNewEntry(new Date(parseInt(moment(e.target.value, 'YYYY:MM:DD').format('x'))).toISOString())
+		setNoEntryDate(date)
 		setEntry(entries.find(e => {
 			let entryDate = moment(e.signIn)
 			if (entryDate.isSame(moment(date), 'day')) return e._id
 		}))
+	}
+
+	const createEntry = () => {
+		axios.post(`https://ms-workit.herokuapp.com/api/entries/add`, { signIn: newEntry }, AuthHeaders).then(res => {
+			setEntry(res.data)
+			history.push(`/dashboard/entries/${res.data._id}`)
+		})
 	}
 
 	return (
@@ -41,6 +49,15 @@ const EntryPicker = props => {
 				<input type="date" onChange={changeHandler} />
 				<button type="submit">Go to entry</button>
 			</form>
+			{error &&
+				<div className="error-create-entry">
+					<p style={{ color: 'red' }}>Entry doesn't exist for {moment(noEntryDate).format('dddd, MMM D, YYYY')}</p>
+					{entry && <p onClick={() => window.location.reload()} style={{ color: '#0063fe', cursor: 'pointer' }}>
+						Click here to reload the page if it doesn't change.
+					</p>}
+					{!entry && <button onClick={() => createEntry()}>Create entry for {moment(noEntryDate).format('dddd, MMM D, YYYY')}</button>}
+				</div>
+			}
 		</div>
 	)
 }
